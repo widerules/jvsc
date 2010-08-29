fc817b80acb7a24f830c20bea7d78121a5d20401
evgenyvinnik <evgenyvinnik@afba0150-abe3-9280-36f7-ce7a5723f633>
2010-08-29 21:46:21 +0000

    Committing new project

---
+++ UbuntuWallpaper/src/ca/jvsh/ubuntu/Ubuntu.java
@@ -0,0 +1,13 @@
+package ca.jvsh.ubuntu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Ubuntu extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
