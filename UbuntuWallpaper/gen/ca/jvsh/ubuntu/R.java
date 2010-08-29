78b301610700eced48a8383168a95775c14ae42a
evgenyvinnik <evgenyvinnik@afba0150-abe3-9280-36f7-ce7a5723f633>
2010-08-29 22:55:51 +0000

    1. implemented empty wallpaper project

--- UbuntuWallpaper/gen/ca/jvsh/ubuntu/R.java
+++ UbuntuWallpaper/gen/ca/jvsh/ubuntu/R.java
@@ -13,11 +13,16 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
-    public static final class layout {
-        public static final int main=0x7f030000;
-    }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        /**  Application name 
+         */
+        public static final int app_name=0x7f040000;
+        public static final int ubuntu_movement_summary=0x7f040003;
+        public static final int ubuntu_movement_title=0x7f040002;
+        public static final int ubuntu_settings=0x7f040001;
+    }
+    public static final class xml {
+        public static final int ubuntu=0x7f030000;
+        public static final int ubuntu_settings=0x7f030001;
     }
 }
