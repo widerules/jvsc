9450f6d3e0169bbb84228cd21f45ec675dfc9a55
evgenyvinnik <evgenyvinnik@afba0150-abe3-9280-36f7-ce7a5723f633>
2011-03-29 16:21:24 +0000

    1. simplified solution

--- TiledPatternLiveWallpaper/src/ca/jvsh/tiledpattern/TiledPatternLiveWallpaper.java
+++ TiledPatternLiveWallpaper/src/ca/jvsh/tiledpattern/TiledPatternLiveWallpaper.java
@@ -6,19 +6,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.GradientDrawable.Orientation;
 import android.os.Handler;
 import android.service.wallpaper.WallpaperService;
 import android.util.DisplayMetrics;
 import android.util.FloatMath;
 import android.view.Display;
-import android.view.MotionEvent;
-import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.WindowManager;
-import android.os.SystemClock;
 
 public class TiledPatternLiveWallpaper extends WallpaperService 
 {
@@ -47,10 +41,8 @@ public class TiledPatternLiveWallpaper extends WallpaperService
 							 implements SharedPreferences.OnSharedPreferenceChangeListener
 	{
 
-		private final Handler		mHandler		= new Handler();
-		//private float				mTouchX			= -1;
-		//private float				mTouchY			= -1;
-		//private final Paint			mPaint			= 
+		private final Handler		mHandler		=   new Handler();
+
 		private final Runnable		mDrawPattern	=   new Runnable()
 														{
 															public void run()
@@ -60,8 +52,6 @@ public class TiledPatternLiveWallpaper extends WallpaperService
 														};
 		private boolean				mVisible;
 		private SharedPreferences	mPreferences;
-
-		private Rect				mRectFrame;
 
 		private final int			PATTERNS = 6;
 		
@@ -90,9 +80,7 @@ public class TiledPatternLiveWallpaper extends WallpaperService
 		
 		private final Paint 		paint = new Paint();
 		private int 				mPreviousOffset;
-		Resources mRes;
-
-		//private long lastVisibilityOnChange;
+		Resources 					mRes;
 
 		TiledPatternEngine()
 		{
@@ -133,14 +121,6 @@ public class TiledPatternLiveWallpaper extends WallpaperService
 			mNextPattern = mCurrentPattern + 1;
 
 			mPreviousOffset = 0;
-			//paint 
-
-			/*final Paint paint = mPaint;
-			paint.setColor(0xffffffff);
-			paint.setAntiAlias(true);
-			paint.setStrokeWidth(2);
-			paint.setStrokeCap(Paint.Cap.ROUND);
-			paint.setStyle(Paint.Style.STROKE);*/
 
 			mPreferences = TiledPatternLiveWallpaper.this.getSharedPreferences(SHARED_PREFS_NAME, 0);
 			mPreferences.registerOnSharedPreferenceChangeListener(this);
@@ -171,7 +151,6 @@ public class TiledPatternLiveWallpaper extends WallpaperService
 		@Override
 		public void onVisibilityChanged(boolean visible)
 		{
-			//lastVisibilityOnChange = SystemClock.elapsedRealtime();
 			mVisible = visible;
 			if (visible)
 			{
@@ -214,47 +193,21 @@ public class TiledPatternLiveWallpaper extends WallpaperService
 		public void onOffsetsChanged(float xOffset, float yOffset, float xStep,
 				float yStep, int xPixels, int yPixels)
 		{
-			//if (SystemClock.elapsedRealtime() - lastVisibilityOnChange > 30)
-				//return;
 			int offset = (int)(xOffset * 100);
 			if(offset%25 == 0 && offset != mPreviousOffset)
 			{
 				mPreviousOffset = offset;
 
-				System.out.println("offset " + offset);
-				System.out.println("onOffsetsChanged xOffset" + xOffset + " yOffset" + yOffset + " xStep" + xStep + " yStep" + yStep + " xPixels" + xPixels + " yPixels" + yPixels);
 				mCurrentPattern = mNextPattern;
 				mNextPattern = mCurrentPattern + 1;
 				if(mNextPattern > 5)
 					mNextPattern = 0;
-				System.out.println("mCurrentPattern " + mCurrentPattern);
-				System.out.println("mNextPattern " + mNextPattern);
 			}
 
 
 			drawFrame();
 			super.onOffsetsChanged(xOffset, yOffset, xStep, yStep, xPixels, yPixels);
 		}
-
-		/*
-		 * Store the position of the touch event so we can use it for drawing
-		 * later
-		 */
-		/*@Override
-		public void onTouchEvent(MotionEvent event)
-		{
-			if (event.getAction() == MotionEvent.ACTION_MOVE)
-			{
-				mTouchX = event.getX();
-				mTouchY = event.getY();
-			}
-			else
-			{
-				mTouchX = -1;
-				mTouchY = -1;
-			}
-			super.onTouchEvent(event);
-		}*/
 
 		/*
 		 * Draw one frame of the animation. This method gets called repeatedly
@@ -273,7 +226,6 @@ public class TiledPatternLiveWallpaper extends WallpaperService
 				{
 					// draw something
 					drawPattern(c);
-					//drawTouchPoint(c);
 				}
 			}
 			finally
@@ -293,8 +245,6 @@ public class TiledPatternLiveWallpaper extends WallpaperService
 		{
 			c.save();
 			c.drawColor(0xff000000);
-
-			//Paint paint = new Paint();
 
 			paint.setAlpha(125);
 			tile_shift_x += movement_speed_x;
@@ -331,41 +281,18 @@ public class TiledPatternLiveWallpaper extends WallpaperService
 			Display display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
 			display.getMetrics(metrics);
 
-			mRectFrame = new Rect(0, 0, metrics.widthPixels, metrics.heightPixels);
-
-			//int rotation = display.getRotation();
-
 			screen_size_x = metrics.widthPixels;
 			screen_size_y = metrics.heightPixels;
-
-			//System.out.println("screen_size_x = " + screen_size_x + ", screen_size_y = "+screen_size_y);
 
 			for(int i = 0; i < PATTERNS; i++)
 			{
 				fit_x[i] = (int)FloatMath.ceil( (float)(screen_size_x) / (float)(tile_size_x[i]) );
 				fit_y[i] = (int)FloatMath.ceil( (float)(screen_size_y) / (float)(tile_size_y[i]) );
 
-				//System.out.println("Pattern " + i+ ", fit_x = " + fit_x[i] + ", fit_y = "+fit_y[i]);
-
 				remain_x[i] = fit_x[i] * tile_size_x[i] - screen_size_x;
 				remain_y[i] = fit_y[i] * tile_size_y[i] - screen_size_y;
 
-				//System.out.println("Pattern " + i+ ", remain_x = " + remain_x[i] + ", remain_y = "+remain_y[i]);
 			}
-
-			/*tile_size_x = mBackgroundImageFar.getWidth();
-			tile_size_y = mBackgroundImageFar.getHeight();
-
-			int fit_x = 
-			
-
-			*/
-			/*if(rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180)
-				mHorizontal = false;
-			else
-				mHorizontal = true;
-
-			*/
 		}
 	}
 }
