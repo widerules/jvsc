64434ce94e3c23e1b9f5e45a183f5629efa4e36e
evgenyvinnik <evgenyvinnik@afba0150-abe3-9280-36f7-ce7a5723f633>
2011-09-10 18:11:31 +0000

    1. implemented 2 tabs in my Audicy application.

--- Audicy/src/ca/jvsh/audicy/Audicy.java
+++ Audicy/src/ca/jvsh/audicy/Audicy.java
@@ -1,13 +1,104 @@
 package ca.jvsh.audicy;
 
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
-import android.os.Bundle;
 
-public class Audicy extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Audicy extends Activity
+{
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		final ActionBar bar = getActionBar();
+		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
+
+		bar.addTab(bar.newTab().setText("Drum Machine")
+				.setTabListener(new TabListener<DrumActivity.DrumFragment>(this,"drum", DrumActivity.DrumFragment.class)));
+		bar.addTab(bar.newTab().setText("Wobble Bass")
+				.setTabListener(new TabListener<WobbleActivity.WobbleFragment>(this, "wobble",WobbleActivity.WobbleFragment.class)));
+
+		if (savedInstanceState != null)
+		{
+			bar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
+		}
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState)
+	{
+		super.onSaveInstanceState(outState);
+		outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
+	}
+
+	public static class TabListener<T extends Fragment> implements
+			ActionBar.TabListener
+	{
+		private final Activity	mActivity;
+		private final String	mTag;
+		private final Class<T>	mClass;
+		private final Bundle	mArgs;
+		private Fragment		mFragment;
+
+		public TabListener(Activity activity, String tag, Class<T> clz)
+		{
+			this(activity, tag, clz, null);
+		}
+
+		public TabListener(Activity activity, String tag, Class<T> clz,
+				Bundle args)
+		{
+			mActivity = activity;
+			mTag = tag;
+			mClass = clz;
+			mArgs = args;
+
+			// Check to see if we already have a fragment for this tab, probably
+			// from a previously saved state. If so, deactivate it, because our
+			// initial state is that a tab isn't shown.
+			mFragment = mActivity.getFragmentManager().findFragmentByTag(mTag);
+			if (mFragment != null && !mFragment.isDetached())
+			{
+				FragmentTransaction ft = mActivity.getFragmentManager()
+						.beginTransaction();
+				ft.detach(mFragment);
+				ft.commit();
+			}
+		}
+
+		public void onTabSelected(Tab tab, FragmentTransaction ft)
+		{
+			if (mFragment == null)
+			{
+				mFragment = Fragment.instantiate(mActivity, mClass.getName(),
+						mArgs);
+				ft.add(android.R.id.content, mFragment, mTag);
+			}
+			else
+			{
+				ft.attach(mFragment);
+			}
+		}
+
+		public void onTabUnselected(Tab tab, FragmentTransaction ft)
+		{
+			if (mFragment != null)
+			{
+				ft.detach(mFragment);
+			}
+		}
+
+		public void onTabReselected(Tab tab, FragmentTransaction ft)
+		{
+			Toast.makeText(mActivity, "Reselected!", Toast.LENGTH_SHORT).show();
+		}
+	}
 }
