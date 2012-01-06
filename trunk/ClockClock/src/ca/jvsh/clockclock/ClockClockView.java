9bfa900ee3e989d35575c9b73c62f7437796eebe
evgenyvinnik <evgenyvinnik@afba0150-abe3-9280-36f7-ce7a5723f633>
2012-01-06 04:17:37 +0000

    1. optimized ClockClock widget 2. changed icon

--- trunk/ClockClock/src/ca/jvsh/clockclock/ClockClockView.java
+++ trunk/ClockClock/src/ca/jvsh/clockclock/ClockClockView.java
@@ -4,29 +4,26 @@ import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.Path.Direction;
-import android.graphics.RectF;
+import android.graphics.RadialGradient;
 import android.text.format.Time;
 import android.util.DisplayMetrics;
 import android.widget.RemoteViews;
 
 public class ClockClockView
 {
-	private static final int	HORIZONTAL_DIALS = 2;
-	private static final int	VERTICAL_DIALS = 3;
-	private static final int 	DIAL_ARROWS = 2;
-	
-	private static final int	SEGMENTS = 2;
-	private static final int	DIGIT_PER_SEGMENT = 2;
-	
-	private static final int	CLOCK_DIGITS = SEGMENTS * DIGIT_PER_SEGMENT;
-	
-	private static final int	COORDINATES = 2;
-	
+	private static final int	HORIZONTAL_DIALS		= 2;
+	private static final int	VERTICAL_DIALS			= 3;
+	private static final int	DIAL_ARROWS				= 2;
+
+	private static final int	SEGMENTS				= 2;
+	private static final int	DIGIT_PER_SEGMENT		= 2;
+
+	private static final int	CLOCK_DIGITS			= SEGMENTS * DIGIT_PER_SEGMENT;
+
+	private static final int	COORDINATES				= 2;
+
 	private int					mHeight;
 	private int					mWidth;
 	private float				mDensity;
@@ -42,47 +39,51 @@ public class ClockClockView
 
 	Bitmap						mArrowsBitmap;
 	Canvas						mCanvasArrows;
-	
-	float						mHourHandLength			= 4;
-	float						mMinuteHandLength			= 5;
-	
-	
-
 
 	int[]						mOldDigits				= new int[SEGMENTS];
 	int[]						mCurrentDigits			= new int[SEGMENTS];
 
 	float[][][][]				mTargetAngles			= new float[CLOCK_DIGITS][VERTICAL_DIALS][HORIZONTAL_DIALS][DIAL_ARROWS];
 	float[][][][]				mCurrentAngles			= new float[CLOCK_DIGITS][VERTICAL_DIALS][HORIZONTAL_DIALS][DIAL_ARROWS];
-	float[][][][]				mTurnAngles				= new float[CLOCK_DIGITS][VERTICAL_DIALS][HORIZONTAL_DIALS][DIAL_ARROWS];
+	int[][][][]					mMadeCircle				= new int[CLOCK_DIGITS][VERTICAL_DIALS][HORIZONTAL_DIALS][DIAL_ARROWS];
+	private final float[]		mTurnAngles				= { 22.5f / 12.0f, 22.5f };
+	private final int[]			mHandTurns				= { 1, 12 };
 
 	float[][][][]				mDialCenterCoordinates	= new float[CLOCK_DIGITS][VERTICAL_DIALS][HORIZONTAL_DIALS][COORDINATES];
-	
-	private final float			mOffsetX				= 80;
-	private final float			mOffsetY				= 20;
-	private final float			mDialSize				= 40;
-	private final float			mDialSizeHalf			= mDialSize/2;
-	private final float			mDigitOffsetX			= 10;
 
+	private final float			mOffsetX				= 3;
+	private final float			mOffsetY				= 3;
+	private final float			mDialSize				= 42;
+	private final float			mDialSizeHalf			= mDialSize / 2;
+	private final float			mTensOffsetX			= 21;
+	private final float			mDigitOffsetX			= 7;
+	private final float[]		mHandLengths			= { mDialSizeHalf * 0.8f, mDialSizeHalf * 0.9f };
+	private final float			eps						= 0.4f;
+
+	private final float			mFrameWidth				= 3;
 
 	Time						mCurrentTime			= new Time();
 
-	int							mHandsColor				= 0xFF000000;
-	boolean						mReadSettings = false;
+	int							mHandsColor				= 0xFF010101;
+	boolean						mNeedRedraw				= false;
+	SharedPreferences 			mPrefs;
 
 	public ClockClockView(Context context, int widgetId)
 	{
 		DisplayMetrics metrics = ClockClockWidgetApp.getMetrics();
 
 		mDensity = metrics.density;
-		mWidth = (int) (370 * metrics.density);
-		mHeight = (int) (100 * metrics.density);
+		mWidth = (int) (400 * metrics.density);
+		mHeight = (int) (200 * metrics.density);
 
 		mWidgetId = widgetId;
+
+		mPrefs = getContext().getSharedPreferences("prefs", 0);
 
 		//set Paint variables
 		mPaint.setAntiAlias(true);
 		mPaint.setDither(true);
+		mPaint.setStrokeWidth(mFrameWidth * mDensity);
 
 		mMainBitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
 		mCanvasMain = new Canvas(mMainBitmap);
@@ -90,12 +91,8 @@ public class ClockClockView
 		mArrowsBitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
 		mCanvasArrows = new Canvas(mArrowsBitmap);
 
-	
 		mCoverBitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
 		Canvas canvasCoverBitmap = new Canvas(mCoverBitmap);
-
-		mPaint.setColor(0xFF0C141F);
-
 
 		//set arrow 
 		for (int i = 0; i < CLOCK_DIGITS; i++)
@@ -105,21 +102,35 @@ public class ClockClockView
 				for (int k = 0; k < HORIZONTAL_DIALS; k++)
 				{
 
-					mDialCenterCoordinates[i][j][k][0] = mOffsetX + k * mDialSize;
-					mDialCenterCoordinates[i][j][k][0] += mDigitOffsetX * (i % DIGIT_PER_SEGMENT );
-					mDialCenterCoordinates[i][j][k][1] = mOffsetY + j * mDialSize;
-	
-					canvasCoverBitmap.drawRect(mDialCenterCoordinates[i][j][k][0]-mDialSizeHalf, mDialCenterCoordinates[i][j][k][1]-mDialSizeHalf,
-							mDialCenterCoordinates[i][j][k][0]+mDialSizeHalf, mDialCenterCoordinates[i][j][k][1]+mDialSizeHalf, mPaint);
+					mDialCenterCoordinates[i][j][k][0] = mOffsetX + (k + i * HORIZONTAL_DIALS) * (mDialSize + mFrameWidth) + mDialSizeHalf;
+					mDialCenterCoordinates[i][j][k][0] += mDigitOffsetX * (i % DIGIT_PER_SEGMENT);
+					mDialCenterCoordinates[i][j][k][0] += mTensOffsetX * (i / DIGIT_PER_SEGMENT);
+					mDialCenterCoordinates[i][j][k][1] = mOffsetY + j * (mDialSize + mFrameWidth) + mDialSizeHalf;
 
-					
 					for (int l = 0; l < COORDINATES; l++)
 					{
 						mDialCenterCoordinates[i][j][k][l] *= mDensity;
 					}
+
+					mPaint.setColor(0xFF000000);
+					mPaint.setStyle(Paint.Style.STROKE);
+					mPaint.setShader(null);
+					canvasCoverBitmap.drawRect(mDialCenterCoordinates[i][j][k][0] - mDialSizeHalf * mDensity, mDialCenterCoordinates[i][j][k][1] - mDialSizeHalf, mDialCenterCoordinates[i][j][k][0] + mDialSizeHalf, mDialCenterCoordinates[i][j][k][1] + mDialSizeHalf, mPaint);
+
+					mPaint.setStyle(Paint.Style.FILL);
+					mPaint.setShader(new RadialGradient(mDialCenterCoordinates[i][j][k][0], mDialCenterCoordinates[i][j][k][1], mDialSize + mDialSizeHalf, 0xFFFFFFF0, 0xFFB5E6B5, android.graphics.Shader.TileMode.CLAMP));
+
+					canvasCoverBitmap.drawRect(mDialCenterCoordinates[i][j][k][0] - mDialSizeHalf * mDensity, mDialCenterCoordinates[i][j][k][1] - mDialSizeHalf * mDensity, mDialCenterCoordinates[i][j][k][0] + mDialSizeHalf * mDensity, mDialCenterCoordinates[i][j][k][1] + mDialSizeHalf * mDensity, mPaint);
+
 				}
 			}
 		}
+
+		mPaint.setShader(null);
+		mPaint.setColor(mHandsColor);
+		mPaint.setStyle(Paint.Style.STROKE);
+		mPaint.setStrokeWidth(1.2f * mFrameWidth * mDensity);
+
 	}
 
 	public Context getContext()
@@ -127,31 +138,28 @@ public class ClockClockView
 		return (ClockClockWidgetApp.getApplication());
 	}
 
-
-
 	public void Redraw(AppWidgetManager appWidgetManager)
 	{
-		if (!mReadSettings)
+		if (mHandsColor == 0xFF010101)
 		{
-			SharedPreferences prefs = getContext().getSharedPreferences("prefs", 0);
-			mHandsColor = prefs.getInt("color" + mWidgetId, 0xff6FC3DF);
-			mReadSettings = true;
-			
-			mPaint.setStrokeWidth(4 * mDensity);
+			mHandsColor = mPrefs.getInt("color" + mWidgetId, 0xFF010101);
 			mPaint.setColor(mHandsColor);
-
 		}
 
 		RemoteViews rviews = new RemoteViews(getContext().getPackageName(), R.layout.clockclock_widget);
 
-		mCanvasMain.drawColor(0, android.graphics.PorterDuff.Mode.CLEAR);
-		mCanvasArrows.drawColor(0, android.graphics.PorterDuff.Mode.CLEAR);
+		if (adjustAngles())
+		{
 
-		mCanvasMain.drawBitmap(mCoverBitmap, 0, 0, mPaint);
+			mCanvasMain.drawColor(0, android.graphics.PorterDuff.Mode.CLEAR);
+			mCanvasArrows.drawColor(0, android.graphics.PorterDuff.Mode.CLEAR);
 
-		drawArrowHands();
+			mCanvasMain.drawBitmap(mCoverBitmap, 0, 0, mPaint);
 
-		mCanvasMain.drawBitmap(mArrowsBitmap, 0, 0, mPaint);
+			drawArrowHands();
+
+			mCanvasMain.drawBitmap(mArrowsBitmap, 0, 0, mPaint);
+		}
 
 		rviews.setImageViewBitmap(R.id.block, mMainBitmap);
 
@@ -159,205 +167,268 @@ public class ClockClockView
 	}
 
 	//drawing functions
-	private static void setAngles(int digit, float[][][] handlesAngles)
+	private static void setDigitAngles(int digit, float[][][] handlesAngles)
 	{
 		switch (digit)
 		{
 			case 0:
 			{
-				//[VERTICAL_DIALS][HORIZONTAL_DIALS][DIAL_ARROWS]
-				//handlesAngles[0][0][1]
+				handlesAngles[0][0][0] = 90;
+				handlesAngles[0][0][1] = 0;
+				handlesAngles[0][1][0] = 90;
+				handlesAngles[0][1][1] = 180;
+
+				handlesAngles[1][0][0] = 270;
+				handlesAngles[1][0][1] = 90;
+				handlesAngles[1][1][0] = 270;
+				handlesAngles[1][1][1] = 90;
+
+				handlesAngles[2][0][0] = 270;
+				handlesAngles[2][0][1] = 0;
+				handlesAngles[2][1][0] = 270;
+				handlesAngles[2][1][1] = 180;
 				break;
 			}
 			case 1:
 			{
-			
+				handlesAngles[0][0][0] = handlesAngles[0][0][1] = 135;
+				handlesAngles[0][1][0] = handlesAngles[0][1][1] = 90;
+
+				handlesAngles[1][0][0] = handlesAngles[1][0][1] = 135;
+				handlesAngles[1][1][0] = 270;
+				handlesAngles[1][1][1] = 90;
+
+				handlesAngles[2][0][0] = handlesAngles[2][0][1] = 135;
+				handlesAngles[2][1][0] = handlesAngles[2][1][1] = 270;
 
 				break;
 			}
 			case 2:
 			{
-				
+				handlesAngles[0][0][0] = handlesAngles[0][0][1] = 0;
+				handlesAngles[0][1][0] = 90;
+				handlesAngles[0][1][1] = 180;
+
+				handlesAngles[1][0][0] = 90;
+				handlesAngles[1][0][1] = 0;
+				handlesAngles[1][1][0] = 270;
+				handlesAngles[1][1][1] = 180;
+
+				handlesAngles[2][0][0] = 270;
+				handlesAngles[2][0][1] = 0;
+				handlesAngles[2][1][0] = handlesAngles[2][1][1] = 180;
 				break;
 			}
 			case 3:
 			{
-				
+				handlesAngles[0][0][0] = handlesAngles[0][0][1] = 0;
+				handlesAngles[0][1][0] = 90;
+				handlesAngles[0][1][1] = 180;
+
+				handlesAngles[1][0][0] = handlesAngles[1][0][1] = 0;
+				handlesAngles[1][1][0] = 270;
+				handlesAngles[1][1][1] = 180;
+
+				handlesAngles[2][0][0] = handlesAngles[2][0][1] = 0;
+				handlesAngles[2][1][0] = 270;
+				handlesAngles[2][1][1] = 180;
 				break;
 			}
 			case 4:
 			{
-				
+				handlesAngles[0][0][0] = handlesAngles[0][0][1] = 90;
+				handlesAngles[0][1][0] = handlesAngles[0][1][1] = 90;
+
+				handlesAngles[1][0][0] = 270;
+				handlesAngles[1][0][1] = 0;
+				handlesAngles[1][1][0] = 270;
+				handlesAngles[1][1][1] = 90;
+
+				handlesAngles[2][0][0] = handlesAngles[2][0][1] = 135;
+				handlesAngles[2][1][0] = handlesAngles[2][1][1] = 270;
+
 				break;
 			}
 			case 5:
 			{
-				
+				handlesAngles[0][0][0] = 90;
+				handlesAngles[0][0][1] = 0;
+				handlesAngles[0][1][0] = handlesAngles[0][1][1] = 180;
+
+				handlesAngles[1][0][0] = 270;
+				handlesAngles[1][0][1] = 0;
+				handlesAngles[1][1][0] = 90;
+				handlesAngles[1][1][1] = 180;
+
+				handlesAngles[2][0][0] = handlesAngles[2][0][1] = 0;
+				handlesAngles[2][1][0] = 270;
+				handlesAngles[2][1][1] = 180;
 				break;
 			}
 			case 6:
 			{
-				
+				handlesAngles[0][0][0] = 90;
+				handlesAngles[0][0][1] = 0;
+				handlesAngles[0][1][0] = handlesAngles[0][1][1] = 180;
+
+				handlesAngles[1][0][0] = 270;
+				handlesAngles[1][0][1] = 90;
+				handlesAngles[1][1][0] = 90;
+				handlesAngles[1][1][1] = 180;
+
+				handlesAngles[2][0][0] = 270;
+				handlesAngles[2][0][1] = 0;
+				handlesAngles[2][1][0] = 270;
+				handlesAngles[2][1][1] = 180;
 				break;
 			}
 			case 7:
 			{
-				
+				handlesAngles[0][0][0] = handlesAngles[0][0][1] = 0;
+				handlesAngles[0][1][0] = 90;
+				handlesAngles[0][1][1] = 180;
+
+				handlesAngles[1][0][0] = handlesAngles[1][0][1] = 135;
+				handlesAngles[1][1][0] = 270;
+				handlesAngles[1][1][1] = 90;
+
+				handlesAngles[2][0][0] = handlesAngles[2][0][1] = 135;
+				handlesAngles[2][1][0] = handlesAngles[2][1][1] = 270;
 				break;
 			}
 			case 8:
 			{
-				
+				handlesAngles[0][0][0] = 90;
+				handlesAngles[0][0][1] = 0;
+				handlesAngles[0][1][0] = 90;
+				handlesAngles[0][1][1] = 180;
+
+				handlesAngles[1][0][0] = 270;
+				handlesAngles[1][0][1] = 0;
+				handlesAngles[1][1][0] = 270;
+				handlesAngles[1][1][1] = 180;
+
+				handlesAngles[2][0][0] = 270;
+				handlesAngles[2][0][1] = 0;
+				handlesAngles[2][1][0] = 270;
+				handlesAngles[2][1][1] = 180;
 				break;
 			}
 			case 9:
 			{
-				
+				handlesAngles[0][0][0] = 90;
+				handlesAngles[0][0][1] = 0;
+				handlesAngles[0][1][0] = 90;
+				handlesAngles[0][1][1] = 180;
+
+				handlesAngles[1][0][0] = 270;
+				handlesAngles[1][0][1] = 0;
+				handlesAngles[1][1][0] = 270;
+				handlesAngles[1][1][1] = 90;
+
+				handlesAngles[2][0][0] = handlesAngles[2][0][1] = 0;
+				handlesAngles[2][1][0] = 270;
+				handlesAngles[2][1][1] = 180;
 				break;
 			}
 		}
 
 	}
 
-	private void drawArrowHands()
+	private boolean adjustAngles()
 	{
+		mNeedRedraw = false;
 
-	/*	mCurrentTime.setToNow();
+		mCurrentTime.setToNow();
 
 		mCurrentDigits[0] = mCurrentTime.hour;
 		mCurrentDigits[1] = mCurrentTime.minute;
-				
 
-		if (mCurrentDigits[0] != mOldDigits[0] || mCurrentDigits[1] != mOldDigits[1] || mCurrentDigits[2] != mOldDigits[2])
+		if (mCurrentDigits[0] != mOldDigits[0] || mCurrentDigits[1] != mOldDigits[1])
 		{
+			mNeedRedraw = true;
+
 			mOldDigits[0] = mCurrentDigits[0];
 			mOldDigits[1] = mCurrentDigits[1];
-			mOldDigits[2] = mCurrentDigits[2];
 
-			setAngles(mCurrentDigits[0], mTargetAngles[0], mTargetAngles[1], 0);
-			setAngles(mCurrentDigits[1], mTargetAngles[2], mTargetAngles[3], mCurrentDigits[0] % 10);
-			setAngles(mCurrentDigits[2], mTargetAngles[4], mTargetAngles[5], mCurrentDigits[1] % 10);
-
-			for (int j = 0; j < 6; j++)
+			setDigitAngles(mCurrentDigits[0] / 10, mTargetAngles[0]);
+			setDigitAngles(mCurrentDigits[0] % 10, mTargetAngles[1]);
+			setDigitAngles(mCurrentDigits[1] / 10, mTargetAngles[2]);
+			setDigitAngles(mCurrentDigits[1] % 10, mTargetAngles[3]);
+			for (int i = 0; i < CLOCK_DIGITS; i++)
 			{
-				for (int i = 0; i < 17; i++)
+				for (int j = 0; j < VERTICAL_DIALS; j++)
 				{
-					if (Math.abs(mCurrentAngles[j][i] - mTargetAngles[j][i]) > 5)
+					for (int k = 0; k < HORIZONTAL_DIALS; k++)
 					{
-						mTurnAngles[j][i] = (mTargetAngles[j][i] - mCurrentAngles[j][i]) / 3.0f;
+						for (int l = 0; l < DIAL_ARROWS; l++)
+						{
+							mMadeCircle[i][j][k][l] = 0;
+						}
 					}
 				}
 			}
+
 		}
 
-		for (int j = 0; j < 6; j++)
+		for (int i = 0; i < CLOCK_DIGITS; i++)
 		{
-			for (int i = 0; i < 17; i++)
+			for (int j = 0; j < VERTICAL_DIALS; j++)
 			{
-				if (Math.abs(mCurrentAngles[j][i] - mTargetAngles[j][i]) > 5)
+				for (int k = 0; k < HORIZONTAL_DIALS; k++)
 				{
-					mCurrentAngles[j][i] += mTurnAngles[j][i];
-				}
-				else
-				{
-					mCurrentAngles[j][i] = mTargetAngles[j][i];
-				}
-
-			}
-		}
-
-		{
-			mLaserPath.reset();
-			//propagate laser beam
-			{
-				mLaserX = 5 * scale;
-				mLaserY = (189 + mOffsetY) * digitscale;
-
-				mLaserPath.moveTo(mLaserX, mLaserY);
-				mLaserRotation = 0;
-
-				mDigit = -1;
-				mMirror = -1;
-				//tens
-				while (mLaserX >= (5 * scale) && mLaserX <= ((mWidth - 5) * scale) && mLaserY > (5 * scale) && mLaserY < ((mHeight - 5) * scale))
-				{
-
-					mLaserX += (float) (Math.cos(mLaserRotation / 180.0f * Math.PI));
-					mLaserY += (float) (Math.sin(mLaserRotation / 180.0f * Math.PI));
-					mMirrorFound = false;
-
-					for (int j = 0; j < 6; j++)
+					for (int l = 0; l < DIAL_ARROWS; l++)
 					{
-						for (int i = 0; i < 17; i++)
+						if (mMadeCircle[i][j][k][l] < mHandTurns[l])
 						{
-							if (Math.abs(mMirrorCoordinates[j][0][i] - mLaserX) < eps && Math.abs(mMirrorCoordinates[j][1][i] - mLaserY) < eps)
-							{
-								if (mMirror != i || mDigit != j)
-								{
-									mMirror = i;
-									mDigit = j;
-									mMirrorFound = true;
-									mLaserX = mMirrorCoordinates[j][0][i];
-									mLaserY = mMirrorCoordinates[j][1][i];
+							mCurrentAngles[i][j][k][l] += mTurnAngles[l];
+							mCurrentAngles[i][j][k][l] %= 360;
+							mNeedRedraw = true;
+						}
 
-									if (mTimeFormat == 2)
-									{
-										if (mCurrentAngles[j][i] == 0 || mCurrentAngles[j][i] == 90 || mCurrentAngles[j][i] == 45 || mCurrentAngles[j][i] == -45 || mCurrentAngles[j][i] == 22.5 || mCurrentAngles[j][i] == -22.5 || mCurrentAngles[j][i] == -90 || mCurrentAngles[j][i] == -67.5 || mCurrentAngles[j][i] == 67.5)
-										{
-											mLaserPath.lineTo(mLaserX, mLaserY);
-											mLaserRotation = 2.0f * mCurrentAngles[j][i] - mLaserRotation;
-										}
-									}
-									else
-									{
-										mLaserPath.lineTo(mLaserX, mLaserY);
-										mLaserRotation = 2.0f * mTargetAngles[j][i] - mLaserRotation;
-									}
-									break;
-								}
+						if (l == 1)
+						{
+							if (mMadeCircle[i][j][k][0] >= mHandTurns[0] && mMadeCircle[i][j][k][1] < mHandTurns[1])
+							{
+								mMadeCircle[i][j][k][1] = mHandTurns[1] - 1;
 							}
 						}
 
-						if (mMirrorFound)
-							break;
+						if (Math.abs(mCurrentAngles[i][j][k][l] - mTargetAngles[i][j][k][l]) < eps)
+						{
+							mMadeCircle[i][j][k][l]++;
+						}
+
 					}
 				}
-				mLaserPath.lineTo(mLaserX, mLaserY);
 			}
+		}
 
-			for (int j = 0; j < 6; j++)
+		return mNeedRedraw;
+	}
+
+	private void drawArrowHands()
+	{
+		for (int i = 0; i < CLOCK_DIGITS; i++)
+		{
+			for (int j = 0; j < VERTICAL_DIALS; j++)
 			{
-				for (int i = 0; i < 17; i++)
+				for (int k = 0; k < HORIZONTAL_DIALS; k++)
 				{
-					if (Math.abs(mCurrentAngles[j][i] - mTargetAngles[j][i]) > 5)
+					for (int l = 0; l < DIAL_ARROWS; l++)
 					{
-						mCurrentAngles[j][i] += mTurnAngles[j][i];
-					}
-					else
-					{
-						mCurrentAngles[j][i] = mTargetAngles[j][i];
-					}
+						mDialCenterCoordinates[i][j][k][l] *= mDensity;
 
-					drawPixelMirror(mMirrorCoordinates[j][0][i], mMirrorCoordinates[j][1][i], mCurrentAngles[j][i]);
-
+						drawClockHand(mDialCenterCoordinates[i][j][k][0], mDialCenterCoordinates[i][j][k][1], mCurrentAngles[i][j][k][l], mHandLengths[l]);
+					}
 				}
 			}
-
-			mPaint.setColor(mLaserColor);
-			mPaint.setStyle(Paint.Style.STROKE);
-			mCanvasLaser.drawPath(mLaserPath, mPaint);
-			mPaint.setStyle(Paint.Style.FILL);
-
-			mPaintBlur.setColor(mLaserColor & 0x00FFFFFF + 0x99000000);
-			mCanvasLaser.drawPath(mLaserPath, mPaintBlur);
-		}*/
+		}
 	}
 
 	private void drawClockHand(float centerX, float centerY, float angleDeg, float handLength)
 	{
 
-		mCanvasArrows.drawLine(centerX, centerY,
-				centerX + handLength * (float) Math.cos(angleDeg * Math.PI / 180.0f)* mDensity, 
-				centerY + handLength * (float) Math.sin(angleDeg * Math.PI / 180.0f)* mDensity, mPaint);
+		mCanvasArrows.drawLine(centerX, centerY, centerX + handLength * (float) Math.cos(angleDeg * Math.PI / 180.0f) * mDensity, centerY + handLength * (float) Math.sin(angleDeg * Math.PI / 180.0f) * mDensity, mPaint);
 	}
 }
