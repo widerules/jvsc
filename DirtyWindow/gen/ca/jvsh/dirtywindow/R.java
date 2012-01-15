57d39b31e41996c07f0a4031555392a03c418829
evgenyvinnik <evgenyvinnik@afba0150-abe3-9280-36f7-ce7a5723f633>
2012-01-15 01:41:26 +0000

    1. added new window images 2. removed unused package 3. added new icon

--- DirtyWindow/gen/ca/jvsh/dirtywindow/R.java
+++ DirtyWindow/gen/ca/jvsh/dirtywindow/R.java
@@ -16,7 +16,13 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int window=0x7f020001;
-        public static final int wipesponge=0x7f020002;
+        public static final int window1=0x7f020002;
+        public static final int window2=0x7f020003;
+        public static final int window3=0x7f020004;
+        public static final int window4=0x7f020005;
+        public static final int window5=0x7f020006;
+        public static final int window6=0x7f020007;
+        public static final int wipesponge=0x7f020008;
     }
     public static final class raw {
         public static final int window=0x7f030000;
