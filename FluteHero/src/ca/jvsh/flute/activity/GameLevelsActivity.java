4cf8c9e17e477f07977f9b61b8a0d5496f932c4c
evgenyvinnik <evgenyvinnik@afba0150-abe3-9280-36f7-ce7a5723f633>
2012-03-16 19:11:56 +0000

    1. Added Advertisements 2. Added game element 3. added floating menu 4. added blow level meter

---
+++ FluteHero/src/ca/jvsh/flute/activity/GameLevelsActivity.java
@@ -0,0 +1,13 @@
+package ca.jvsh.flute.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class GameLevelsActivity  extends FragmentActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+     }
+}
