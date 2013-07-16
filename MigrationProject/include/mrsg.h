7f2ac40937b99025b0aaacd8fe6edf0662fdd367
evgenyvinnik@gmail.com <evgenyvinnik@gmail.com@afba0150-abe3-9280-36f7-ce7a5723f633>
2013-07-16 20:18:37 +0000

    1. fixed signed/unsigned warnings: now functions return unsigned integers and variables are also unsigned integers whether it is possible

--- MigrationProject/include/mrsg.h
+++ MigrationProject/include/mrsg.h
@@ -28,12 +28,12 @@ enum phase_e {
 
 void MRSG_init (void);
 
-int MRSG_main (const char* plat, const char* depl, const char* conf);
+int MRSG_main (const char* plat, const char* conf);
 
 void MRSG_set_task_cost_f ( double (*f)(enum phase_e phase, size_t tid, size_t wid) );
 
-void MRSG_set_dfs_f ( void (*f)(char** dfs_matrix, size_t chunks, size_t workers, int replicas) );
+void MRSG_set_dfs_f ( void (*f)(char** dfs_matrix, size_t chunks, size_t workers, unsigned int replicas) );
 
-void MRSG_set_map_output_f ( int (*f)(size_t mid, size_t rid) );
+void MRSG_set_map_output_f ( size_t (*f)(size_t mid, size_t rid) );
 
 #endif /* !MRSG_H */
